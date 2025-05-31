64228cf14e4fc7920fe98349e8505f57125c1a79
Erick Grisoste <grisosteerick25@gmail.com>
2025-05-31 20:11:49 -0300

    validação

--- cadastro.js
+++ cadastro.js
@@ -4,24 +4,51 @@ const botao = document.getElementById('btnCadastrar');
 botao.addEventListener('click',
     function () {
         let produtos = JSON.parse(localStorage.getItem("produtos")) || []; //le o texto salvo no localStorage e transforma em um array de objts, se estiver vazio transforma em array vazio
+        const inputNome = document.getElementById('nome');
+        const inputPreco = document.getElementById('preco');
 
         const produto = { //criando um json com os valores digitados no input nome e preço
             nome: document.getElementById('nome').value,
             preco: document.getElementById('preco').value
         }
 
-        if (!produto.nome.trim() || !produto.preco.trim()) {
+        const preco = parseFloat(document.getElementById('preco').value);
+
+        if (!produto.nome.trim() || !produto.preco.trim()) { //evita que salve se os valores estiverem vazios
+            inputNome.value === "" ? inputNome.style.border = "1px solid red" : inputNome.style.border = "";
+            inputPreco.value === "" ? inputPreco.style.border = "1px solid red" : inputPreco.style.border = "";
             alert("Preencha todos os campos!");
+            return;
+        } else if(produtos.some(p => (p.nome.trim() === produto.nome.trim()) && p.preco === produto.preco)){ //impede cadastro de produtos repetidos
+            alert("Produto já existente na tabela. Tente novamente.");
+
+            inputNome.style.border = "1px solid red";
+            inputPreco.style.border = "1px solid red";
+            
+            return;
+        } else if(isNaN(preco) || preco <= 0){ //verifica se o preço é um número e se é maior que 0
+            alert("Preço inválido!");
+
+            inputPreco.style.border = "1px solid red";
+
             return;
         }
 
         const indexEditando = document.getElementById('indexEditar').value; //pega o valor do input de editar
+        const sucesso = document.getElementById('msg-sucesso');
+        sucesso.innerHTML = "";
 
         if (indexEditando !== "") {
             produtos[indexEditando] = produto;
             document.getElementById('indexEditar').value = ""; //zerando o input de editar
+            const msg = document.createElement('p');
+            msg.innerHTML = `Produto <strong>${produto.nome}</strong> editado com sucesso!`; //criando uma mensagem de sucesso
+            sucesso.appendChild(msg);
         } else {
             produtos.push(produto); //add o produto criado no fim do array;
+            const msg = document.createElement('p');
+            msg.innerHTML = `Produto <strong>${produto.nome}</strong> cadastrado com sucesso!`; //criando uma mensagem de sucesso
+            sucesso.appendChild(msg);
         }
 
         let listaDeProdutos = JSON.stringify(produtos); //transforma o array em texto json
@@ -29,7 +56,10 @@ botao.addEventListener('click',
 
         document.getElementById('nome').value = ""; //zerando os inputs
         document.getElementById('preco').value = "";
-
+        inputNome.style.border = ""; //zerando as bordas de erro
+        inputPreco.style.border = "";
+        botao.innerText = "Cadastrar";
+        
         listar();
     }
 );
@@ -55,6 +85,7 @@ function listar() {
 }
 
 function editarProduto(index) {
+    botao.innerText = "Salvar Edição";
     const listaDeProdutosCad = JSON.parse(localStorage.getItem("produtos")) || [];
     const produto = listaDeProdutosCad[index];
     //preenchendo os inputs com os valores do produto que vai ser editado
